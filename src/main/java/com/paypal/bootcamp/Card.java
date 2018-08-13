1d140f2f3a0df6af8e86f9907dd3c1a4c63f7806
Mallikarjun Swamy <mswami032@gmail.com>
2018-08-13 12:35:03 +0530

    add Card and Pack member variables

--- src/main/java/com/paypal/bootcamp/Card.java
+++ src/main/java/com/paypal/bootcamp/Card.java
@@ -1,4 +1,63 @@
 package com.paypal.bootcamp;
 
 public class Card {
+    static enum Suit { SPADE, HEART, DIAMOND, CLUB, JOKER};
+    private Suit suit;
+    private int seq;
+    private int rank;
+    final int SUITCOUNT = 13;
+    final int JOKERRANK = 52;
+
+    public Card(int seq) {
+        if(seq >= JOKERRANK) {
+            this.seq = JOKERRANK;
+            this.rank = SUITCOUNT;
+            this.suit = Suit.JOKER;
+        }else {
+            this.seq = seq;
+            this.rank = seq%SUITCOUNT;
+            this.suit = Suit.values()[seq/SUITCOUNT];
+        }
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+
+    public void setSuit(Suit suit) {
+        this.suit = suit;
+    }
+
+    public int getSeq() {
+        return seq;
+    }
+
+    public void setSeq(int seq) {
+        this.seq = seq;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) {
+        this.rank = rank;
+    }
+
+
+
+    public String toString() {
+
+        if (suit.equals(Suit.SPADE))
+            return rank + " " + (char) '\u2660';
+        else if (suit.equals(Suit.DIAMOND))
+            return rank + " " + (char) '\u2666';
+        else if (suit.equals(Suit.CLUB))
+            return rank + " " + (char) '\u2663';
+        else if (suit.equals(Suit.HEART))
+            return rank + " " + (char) '\u2764';
+
+        return rank + " " + "JOKER";
+    }
 }
+
--- src/main/java/com/paypal/bootcamp/Deck.java
+++ src/main/java/com/paypal/bootcamp/Deck.java
@@ -1,4 +1,22 @@
 package com.paypal.bootcamp;
 
-public class Deck {
+import java.util.ArrayList;
+import java.util.Collections;
+
+public abstract class Deck {
+
+    private ArrayList<Pack> packs;
+
+    public Deck(int noOfPacks) {
+        packs = new ArrayList<Pack>(noOfPacks);
+    }
+
+
+
+    public void shuffle() {
+        for(Pack p: packs)
+            p.shuffle();
+
+        Collections.shuffle(packs);
+    }
 }
